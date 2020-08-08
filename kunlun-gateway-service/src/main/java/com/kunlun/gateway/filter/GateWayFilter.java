f99d673bf9d373493723a7b395c52064e65b1f07
xuesjie <sjxue126@126.com>
2020-08-09 00:37:20 +0800

    2020-08-09 filter重新定义

--- kunlun-gateway-service/src/main/java/com/kunlun/gateway/filter/GateWayFilter.java
+++ kunlun-gateway-service/src/main/java/com/kunlun/gateway/filter/GateWayFilter.java
@@ -19,6 +19,7 @@ import java.util.Map;
  * zuul过滤器
  */
 public class GateWayFilter extends ZuulFilter {
+
     private Logger log = LogManager.getLogger();
 
     @Autowired
@@ -50,7 +51,6 @@ public class GateWayFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         log.info("Start GateWayFilter run!");
-
         try {
             // 获取请求的上下文类
             RequestContext context = RequestContext.getCurrentContext();
@@ -64,28 +64,28 @@ public class GateWayFilter extends ZuulFilter {
             log.info(String.format("Request protocol: %s, source: %s, gateway: %s, method：%s, address：%s", request.getProtocol(), request.getHeader("origin").split("//")[1], request.getHeader("host"), request.getMethod(), request.getRequestURI()));
 
             // Token校验
-            String token = request.getHeader(shiroConfig.getTokenHeader());
-            String userName = JwtTokenUtil.getTokenInfo(token, "userName");
-            String password = JwtTokenUtil.getTokenInfo(token, "password");
-            String loginTime = JwtTokenUtil.getTokenInfo(token, "loginTime");
-            String redisKey = userName + "_" + loginTime;
-            Map<String, Object> map = (Map<String, Object>) basedataService.get(redisKey, 1);
-            String refreshedToken = (String) map.get("data");
-            boolean jwt = JwtTokenUtil.verify(refreshedToken, userName, password, shiroConfig.getSecret());
-            if (jwt) {
-                // 用户在线操作，Token续期
-                // 此处不能刷新即续期token，否则新的token无法传递到前台，无法保持全局一致；此处不能校验token是否有效，因刷新或续期token无法处理
-                log.info("token ===>>> " + token);
-                String shiroToken = JwtTokenUtil.sign(userName, password, loginTime, shiroConfig.getSecret(), shiroConfig.getExpireTime());
-                log.info("update token ===>>> " + shiroToken);
-                basedataService.set(redisKey, shiroToken, shiroConfig.getExpireTime(), 1);
-            } else {
-                // Token过期后，前台提示用户，并阻止向下游服务继续调用
-                HttpServletResponse response = context.getResponse();
-                response.sendRedirect("/timeout");
-                context.setSendZuulResponse(false);
-                log.info("离开时间太长，请重新登录！");
-            }
+//            String token = request.getHeader(shiroConfig.getTokenHeader());
+//            String userName = JwtTokenUtil.getTokenInfo(token, "userName");
+//            String password = JwtTokenUtil.getTokenInfo(token, "password");
+//            String loginTime = JwtTokenUtil.getTokenInfo(token, "loginTime");
+//            String redisKey = userName + "_" + loginTime;
+//            Map<String, Object> map = (Map<String, Object>) basedataService.get(redisKey, 1);
+//            String refreshedToken = (String) map.get("data");
+//            boolean jwt = JwtTokenUtil.verify(refreshedToken, userName, password, shiroConfig.getSecret());
+//            if (jwt) {
+//                // 用户在线操作，Token续期
+//                // 此处不能刷新即续期token，否则新的token无法传递到前台，无法保持全局一致；此处不能校验token是否有效，因刷新或续期token无法处理
+//                log.info("token ===>>> " + token);
+//                String shiroToken = JwtTokenUtil.sign(userName, password, loginTime, shiroConfig.getSecret(), shiroConfig.getExpireTime());
+//                log.info("update token ===>>> " + shiroToken);
+//                basedataService.set(redisKey, shiroToken, shiroConfig.getExpireTime(), 1);
+//            } else {
+//                // Token过期后，前台提示用户，并阻止向下游服务继续调用
+//                HttpServletResponse response = context.getResponse();
+//                response.sendRedirect("/timeout");
+//                context.setSendZuulResponse(false);
+//                log.info("离开时间太长，请重新登录！");
+//            }
         } catch (Exception e) {
             log.error("GateWayFilter run Error: ", e);
         }
--- kunlun-gateway-service/src/main/java/com/kunlun/gateway/filter/ShiroFilter.java
+++ kunlun-gateway-service/src/main/java/com/kunlun/gateway/filter/ShiroFilter.java
@@ -1,21 +1,20 @@
 package com.kunlun.gateway.filter;
 
+import com.kunlun.gateway.config.BeanUtil;
 import com.kunlun.gateway.model.ShiroConfigModel;
+import com.kunlun.gateway.service.IBasedataService;
+import com.kunlun.gateway.utils.JwtTokenUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * shiro过滤器，认证token
@@ -31,6 +30,13 @@ public class ShiroFilter extends BasicHttpAuthenticationFilter {
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
         log.info("Start ShiroFilter isAccessAllowed");
         try {
+            // ignoreUrls的请求无须Filter处理
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            ShiroConfigModel shiroConfigModel = BeanUtil.getBean(ShiroConfigModel.class);
+            String requestUrl = httpServletRequest.getRequestURI();
+            if (shiroConfigModel.getIgnoreUrls().contains(requestUrl)) {
+                return true;
+            }
             return executeLogin(request, response);
         } catch (Exception e) {
             log.error("ShiroFilter isAccessAllowed Error: ", e);
@@ -41,6 +47,35 @@ public class ShiroFilter extends BasicHttpAuthenticationFilter {
     @Override
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
         log.info("Start ShiroFilter executeLogin");
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        ShiroConfigModel shiroConfigModel = BeanUtil.getBean(ShiroConfigModel.class);
+        IBasedataService basedataService = BeanUtil.getBean(IBasedataService.class);
+
+        // Token校验
+        String token = httpServletRequest.getHeader(shiroConfigModel.getTokenHeader());
+        String userName = JwtTokenUtil.getTokenInfo(token, "userName");
+        String password = JwtTokenUtil.getTokenInfo(token, "password");
+        String loginTime = JwtTokenUtil.getTokenInfo(token, "loginTime");
+        String redisKey = userName + "_" + loginTime;
+        Map<String, Object> map = (Map<String, Object>) basedataService.get(redisKey, 1);
+        String refreshedToken = (String) map.get("data");
+        boolean jwt = JwtTokenUtil.verify(refreshedToken, userName, password, shiroConfigModel.getSecret());
+        if (jwt) {
+            // 用户在线操作，Token续期
+            // 此处不能刷新即续期token，否则新的token无法传递到前台，无法保持全局一致；此处不能校验token是否有效，因刷新或续期token无法处理
+            log.info("token ===>>> " + token);
+            String shiroToken = JwtTokenUtil.sign(userName, password, loginTime, shiroConfigModel.getSecret(), shiroConfigModel.getExpireTime());
+            log.info("update token ===>>> " + shiroToken);
+            basedataService.set(redisKey, shiroToken, shiroConfigModel.getExpireTime(), 1);
+        } else {
+            // Token过期后，前台提示用户，并阻止向下游服务继续调用
+//            httpServletResponse.sendRedirect("/timeout");
+            httpServletResponse.setStatus(888);
+            httpServletResponse.sendError(999);
+            log.info("离开时间太长，请重新登录！");
+        }
         return true;
     }
 
@@ -50,13 +85,15 @@ public class ShiroFilter extends BasicHttpAuthenticationFilter {
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
         log.info("Start ShiroFilter preHandle, config Header!");
+
+        // 请求跨域设置
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
 
-        // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
+        // 跨域时会首先发送一个option请求，给option请求直接返回正常状态
         if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
             httpServletResponse.setStatus(HttpStatus.OK.value());
             return false;
