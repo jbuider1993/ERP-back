df2fa1c833970c42b8066e112dee801c161a60c8
xuesjie <sjxue126@126.com>
2020-08-28 21:24:35 +0800

    2020-08-28 用户统计修改

--- kunlun-basedata-service/src/main/java/com/kunlun/basedata/service/impl/HomeService.java
+++ kunlun-basedata-service/src/main/java/com/kunlun/basedata/service/impl/HomeService.java
@@ -9,20 +9,8 @@ import com.sun.management.OperatingSystemMXBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import oshi.SystemInfo;
-import oshi.hardware.*;
-import oshi.software.os.*;
-import oshi.util.FormatUtil;
-import oshi.util.Util;
-
-import java.io.File;
 import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
