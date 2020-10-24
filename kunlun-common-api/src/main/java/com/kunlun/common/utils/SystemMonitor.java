6db9e1c6f49582f6a7a3b05da81d8a73d4d19b00
xuesjie <xueshengjie@szewec.com>
2020-10-24 18:31:03 +0800

    2020-10-24 获取操作系统信息

--- kunlun-common-api/src/main/java/com/kunlun/common/utils/SystemMonitor.java
+++ kunlun-common-api/src/main/java/com/kunlun/common/utils/SystemMonitor.java
@@ -1,6 +1,6 @@
 package com.kunlun.common.utils;
 
-import com.kunlun.common.model.SystemInfoModel;
+import com.kunlun.common.model.SystemDataModel;
 import com.sun.management.OperatingSystemMXBean;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,46 +21,47 @@ public class SystemMonitor {
 
     private static Logger log = LogManager.getLogger();
 
-    public static SystemInfoModel init() {
-        SystemInfoModel systemInfoModel = new SystemInfoModel();
+    public static SystemDataModel init() {
+        SystemDataModel systemModel = new SystemDataModel();
         try {
             // 操作系统
-            fetchSystemInfo(systemInfoModel);
+            fetchSystemData(systemModel);
 
             // CPU使用情况
             SystemInfo systemInfo = new SystemInfo();
-            fetchCPUInfo(systemInfo, systemInfoModel);
+            fetchCPUData(systemInfo, systemModel);
 
             // 磁盘使用情况
-            fetchDIskInfo(systemInfoModel);
+            fetchDiskData(systemModel);
 
             // Memory使用情况
-            fetchMemoryInfo(systemInfoModel);
+            fetchMemoryData(systemModel);
         } catch (Exception e) {
             log.error("get system information error", e);
         }
-        return systemInfoModel;
+        return systemModel;
     }
 
-    private static void fetchSystemInfo(SystemInfoModel systemInfoModel) {
+    private static void fetchSystemData(SystemDataModel systemModel) {
         String osName = System.getProperty("os.name");
         String startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(ManagementFactory.getRuntimeMXBean().getStartTime()));
-        System.out.println("操作系统:" + osName);
-        System.out.println("程序启动时间:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(ManagementFactory.getRuntimeMXBean().getStartTime())));
-        systemInfoModel.setOsName(osName);
-        systemInfoModel.setStartTime(startTime);
+        log.info("操作系统: " + osName + " 程序启动时间: " + startTime);
+        systemModel.setOsName(osName);
+        systemModel.setStartTime(startTime);
     }
 
-    private static void fetchMemoryInfo(SystemInfoModel systemInfoModel) {
+    private static void fetchMemoryData(SystemDataModel systemModel) {
         OperatingSystemMXBean osmxb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         String totalMemory = new DecimalFormat("#.##").format(osmxb.getTotalPhysicalMemorySize() / 1024.0 / 1024);
-        String usedMemoryA = new DecimalFormat("#.##").format((osmxb.getTotalPhysicalMemorySize() - osmxb.getFreePhysicalMemorySize()) / 1024.0 / 1024);
-        System.out.println("总物理内存大小 ===>>> " + totalMemory + "M");
-        System.out.println("已使用的物理内存 ===>>> " + usedMemoryA + "M");
-        System.out.println("物理内存使用率 ===>>> " + new BigDecimal(usedMemoryA).divide(new BigDecimal(totalMemory), 2, BigDecimal.ROUND_HALF_UP).multiply(new BigDecimal(100)).toString() + "%");
+        String usedMemory = new DecimalFormat("#.##").format((osmxb.getTotalPhysicalMemorySize() - osmxb.getFreePhysicalMemorySize()) / 1024.0 / 1024);
+        String rate = new BigDecimal(usedMemory).divide(new BigDecimal(totalMemory), 2, BigDecimal.ROUND_HALF_UP).multiply(new BigDecimal(100)).toString();
+        log.info("总物理内存大小: " + totalMemory + "M" + " 已使用的物理内存: " + usedMemory + "M" + " 物理内存使用率 " + rate + "%");
+        systemModel.setMaxMemory(totalMemory);
+        systemModel.setUsedMemory(usedMemory);
+        systemModel.setUsedMemoryRate(rate);
     }
 
-    private static void fetchDIskInfo(SystemInfoModel systemInfoModel) {
+    private static void fetchDiskData(SystemDataModel systemModel) {
         String totalDisk = "0", usedDisk = "0";
         File[] files = File.listRoots();
         for (File file : files) {
@@ -71,13 +72,15 @@ public class SystemMonitor {
             usedDisk = new BigDecimal(usedDisk).add(new BigDecimal(total).subtract(new BigDecimal(free))).toString();
             totalDisk = new BigDecimal(totalDisk).add(new BigDecimal(total)).toString();
         }
-        System.out.println("磁盘总大小 ===>>> " + totalDisk + "M");
-        System.out.println("磁盘已使用大小 ===>>> " + usedDisk + "M");
         BigDecimal temp = new BigDecimal(usedDisk).divide(new BigDecimal(totalDisk), 2, BigDecimal.ROUND_HALF_UP);
-        System.out.println("磁盘使用率大小 ===>>> " + temp.multiply(new BigDecimal(100)).toString() + "%");
+        String rate = temp.multiply(new BigDecimal(100)).toString();
+        log.info("磁盘总大小： " + totalDisk + "M" + " 磁盘已使用大小：" + usedDisk + "M" + " 磁盘使用率大小： " + rate + "%");
+        systemModel.setMaxDisk(totalDisk);
+        systemModel.setUsedDisk(usedDisk);
+        systemModel.setUsedDiskRate(rate);
     }
 
-    private static void fetchCPUInfo(SystemInfo systemInfo, SystemInfoModel model) {
+    private static void fetchCPUData(SystemInfo systemInfo, SystemDataModel systemModel) {
         CentralProcessor processor = systemInfo.getHardware().getProcessor();
         long[] prevTicks = processor.getSystemCpuLoadTicks();
         long[] ticks = processor.getSystemCpuLoadTicks();
@@ -102,9 +105,9 @@ public class SystemMonitor {
         String usedSystem = new DecimalFormat("#.##").format(cSys * 1.0 / totalCpu);
         String usedUser = new DecimalFormat("#.##").format(user * 1.0 / totalCpu);
         BigDecimal totalUse = new BigDecimal(usedSystem).add(new BigDecimal(usedUser)).multiply(new BigDecimal(100));
-        System.out.println("CPU核数 ===>>> " + processor.getLogicalProcessorCount());
-        System.out.println("CPU使用率 ===>>> " + totalUse.toString());
-        System.out.println("CPU空闲率 ===>>> " + new DecimalFormat("#.##%").format(idle * 1.0 / totalCpu));
-        model.setCore(processor.getLogicalProcessorCount());
+        String cpuCore = String.valueOf(processor.getLogicalProcessorCount());
+        log.info("CPU核数: " + cpuCore + " CPU使用率: " + totalUse + " CPU空闲率: " + new DecimalFormat("#.##%").format(idle * 1.0 / totalCpu));
+        systemModel.setCpuCore(processor.getLogicalProcessorCount() + "");
+        systemModel.setUsedCpuRate(totalUse.toString());
     }
 }
